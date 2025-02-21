04989c2b874d3213219fe76c5d1e2812abc629a9
Monsieur KOLUJ <misterfrpkoluj@gmail.com>
2025-02-21 20:05:24 +0200

    ONG SEED 8

---
+++ src/assets/components/services/medias.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Footer from "../footer/footer";
+import SEO from "../SEO";
+
+
+const ServiceMedia = () => {
+  // Animation variants
+  const fadeIn = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } },
+  };
+
+  return (
+    <>
+      <section className="relative bg-white dark:bg-black min-h-screen py-12 px-6">
+        
+        {/* OPTIMISATION SEO */}
+      <SEO 
+      title="Services médiatiques | ONG SEED" 
+      description="L'ONG SEED se trouve sur plusieurs plate-formes médiatiques." 
+      keywords="Plate-formes, Médiaques, Médias, Informations, Divertissements" 
+      />
+        {/* OPTIMISATION SEO */}
+
+      {/* Gradient en arrière-plan couvrant tout l'écran */}
+      <div
+          className="absolute inset-0 bg-gradient-to-tr from-blue-950 via-transparent to-red-950 opacity-25 dark:opacity-75"
+          aria-hidden="true"></div>
+
+      <div className="max-w-4xl mx-auto text-center relative">
+        {/* Titre avec animation */}
+        <motion.h2
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: false, amount: 0.5 }}
+          variants={fadeIn}
+          className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-5xl font-extrabold text-teal-600 dark:text-blue-600 mb-12 uppercase"
+        >
+          Services médiatiques
+        </motion.h2>
+      </div>
+    </section>
+    <Footer/>
+    </>
+  );
+};
+
+export default ServiceMedia;
---
+++ src/assets/components/services/seniors.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Footer from "../footer/footer";
+import SEO from "../SEO";
+
+const AideAuxAinés = () => {
+  // Animation variants
+  const fadeIn = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } },
+  };
+
+  return (
+    <>
+      <section className="relative bg-white dark:bg-black min-h-screen py-12 px-6">
+        {/* OPTIMISATION SEO */}
+      <SEO 
+      title="Aide aux ainés | ONG SEED" 
+      description="L'ONG SEED aide non seulement les enfants mais aussi des personnes d'âge mûr." 
+      keywords="Aides, Soutien, Ainés, Prise en charge, Valorisation" 
+      />
+        {/* OPTIMISATION SEO */}
+
+      {/* Gradient en arrière-plan couvrant tout l'écran */}
+      <div
+          className="absolute inset-0 bg-gradient-to-tr from-blue-950 via-transparent to-red-950 opacity-25 dark:opacity-75"
+          aria-hidden="true"></div>
+
+      <div className="max-w-4xl mx-auto text-center relative">
+        {/* Titre avec animation */}
+        <motion.h2
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: false, amount: 0.5 }}
+          variants={fadeIn}
+          className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-5xl font-extrabold text-teal-600 dark:text-blue-600 mb-12 uppercase"
+        >
+          Aide aux ainés
+        </motion.h2>
+      </div>
+    </section>
+    <Footer/>
+    </>
+  );
+};
+
+export default AideAuxAinés;
